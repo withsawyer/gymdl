1c4a46c42233e97df08cd15e8de307477a875cc6
nichuanfang <f18326186224@gmail.com>
2025-10-19 15:32:00 +0800

    feat: 添加基础代码

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/nichuanfang/gymdl/tools"
+
+func main() {
+	println("Hello world")
+	tools.TestTools()
+}
---
+++ tools/load_config.go
@@ -0,0 +1,6 @@
+package tools
+
+// 加载配置
+func TestTools() {
+	println("tools load")
+}
