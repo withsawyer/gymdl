dab8c85ca406a57117ed51125623a112ec663889
nichuanfang <f18326186224@gmail.com>
2025-10-20 18:14:59 +0800

    update go.mod

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	println("hello world")
 	// ## 加载配置
 	// ## 初始化平台: 1.安装可执行程序(yt-dlp ffmpeg gamdl um),2.检查更新可执行程序(yt-dlp gamdl um),3.启动更新定时任务
-	// ## 启动http服务gin
+	// ## 启动http服务gin https://exexute.github.io/2022/07/27/Gin%E6%A1%86%E6%9E%B6-%E4%BB%8E0%E5%BC%80%E5%A7%8B%E6%90%AD%E5%BB%BA-Gin-Web-%E6%9C%8D%E5%8A%A1/
 
 	time.Sleep(time.Hour)
 }
