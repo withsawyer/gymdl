68c56e2f8516e701f75be82d112df653473f160f
nichuanfang <f18326186224@gmail.com>
2025-10-21 16:32:49 +0800

    feat: 新增平台识别模块

--- internal/cron/scheduler.go
+++ internal/cron/scheduler.go
@@ -4,6 +4,8 @@ import (
 	"time"
 
 	"github.com/nichuanfang/gymdl/config"
+	"github.com/nichuanfang/gymdl/core"
+	"github.com/nichuanfang/gymdl/utils"
 )
 
 // InitCron 初始化定时任务
@@ -14,5 +16,7 @@ func InitCron(config *config.Config) {
 	//    - 核心服务(cookiecloud,webdav,ai)健康检查
 	//    - 依赖的可执行文件或者pip更新检测
 	//    - 定期从cookiecloud获取cookie数据并解密为cookie文件
+	platformInfo := core.PlatformInfo()
+	utils.Logger().Infof("当前平台:%s", platformInfo.String())
 	time.Sleep(time.Hour)
 }
