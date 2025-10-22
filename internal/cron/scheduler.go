867304f0632dd7592b7f10b81ae3f5f129e60c7f
nichuanfang <f18326186224@gmail.com>
2025-10-23 01:39:11 +0800

    feat: 依赖项更新设计

--- internal/cron/scheduler.go
+++ internal/cron/scheduler.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"sync"
 	"time"
-	
+
 	"github.com/go-co-op/gocron/v2"
 	"github.com/google/uuid"
 	"github.com/nichuanfang/gymdl/config"
@@ -21,9 +21,9 @@ func InitScheduler(c *config.Config) gocron.Scheduler {
 	logger = utils.Logger()
 	platformInfo := core.PlatformInfo()
 	utils.SugaredLogger().Infof("当前平台: %s", platformInfo.String())
-	
+
 	var startTimes sync.Map
-	
+
 	newScheduler, err := gocron.NewScheduler(
 		gocron.WithLocation(time.Local),
 		gocron.WithGlobalJobOptions(
@@ -53,17 +53,18 @@ func InitScheduler(c *config.Config) gocron.Scheduler {
 	)
 	if err != nil {
 		logger.Fatal("Failed to initialize scheduler", zap.Error(err))
+		return nil
 	}
-	
+
 	registerTasks(c, platformInfo, newScheduler)
 	return newScheduler
 }
 
-//registerTasks 注册定时任务
+// registerTasks 注册定时任务
 func registerTasks(c *config.Config, platform core.Platform, scheduler gocron.Scheduler) {
 	//执行一次依赖安装/更新
-	newTask("installExecutableFile", scheduler, gocron.OneTimeJob(gocron.OneTimeJobStartImmediately()),
-		gocron.NewTask(installExecutableFile, c, platform))
+	newTask("installDependency", scheduler, gocron.OneTimeJob(gocron.OneTimeJobStartImmediately()),
+		gocron.NewTask(installDependency, c, platform))
 	//注册健康检查任务(每分钟执行一次)
 	newTask("healthCheck", scheduler, gocron.DurationJob(time.Minute), gocron.NewTask(healthCheck, c))
 	//注册依赖更新检测任务(6小时)
@@ -73,24 +74,24 @@ func registerTasks(c *config.Config, platform core.Platform, scheduler gocron.Sc
 		gocron.NewTask(syncCookieCloud, c))
 }
 
-//newTask 日志任务创建信息
+// newTask 日志任务创建信息
 func newTask(jobName string, scheduler gocron.Scheduler, jobDefinition gocron.JobDefinition, task gocron.Task) {
 	logger.Info("[Job Added]", zap.String("jobName", jobName))
 	scheduler.NewJob(jobDefinition, task)
 }
 
-//logJobEvent 日志任务执行信息
+// logJobEvent 日志任务执行信息
 func logJobEvent(event, jobName string, jobID uuid.UUID, start *time.Time, err error) {
 	name := jobName
 	if idx := strings.LastIndex(jobName, "."); idx != -1 {
 		name = jobName[idx+1:]
 	}
-	
+
 	fields := []zap.Field{
 		zap.String("job_name", name),
 		zap.String("job_id", jobID.String()),
 	}
-	
+
 	if start != nil {
 		fields = append(fields, zap.String("duration", fmt.Sprintf("%vms", time.Since(*start).Milliseconds())))
 	}
--- internal/cron/tasks.go
+++ internal/cron/tasks.go
@@ -2,37 +2,40 @@ package cron
 
 import (
 	"time"
-	
+
 	"github.com/nichuanfang/gymdl/config"
 	"github.com/nichuanfang/gymdl/core"
 	"github.com/nichuanfang/gymdl/utils"
 )
 
-//installExecutableFile 安装依赖项
-func installExecutableFile(*config.Config, core.Platform) {
-	// 1.todo 如果依赖的可执行文件(如yt-dlp,gamdl,um,ffmpeg)等未安装,执行安装
-	// 2. todo 如果已安装,检查更新(只更新非固定版本的或者经常更新的)
+// installDependency 安装依赖项
+func installDependency(*config.Config, core.Platform) {
+	// todo 如果依赖的可执行文件(如yt-dlp,gamdl,um,ffmpeg)等未安装,执行安装
+
+	//依赖项名称:gamdl
+	//依赖项安装方式:pip
+	//安装指令:pip install -U gamdl
 	logger.Info("开始执行依赖项更新...")
 	time.Sleep(2 * time.Second)
 	logger.Info("依赖项更新完毕")
 	return
 }
 
-//healthCheck 健康检查
-func healthCheck(c *config.Config) {
-	//todo 核心服务(cookiecloud,webdav,ai)健康检查
-	utils.NetworkHealth("健康检查成功")
-	return
-}
-
-//updateDependency 更新依赖
+// updateDependency 更新依赖项
 func updateDependency(c *config.Config, platform core.Platform) {
 	//todo 依赖的可执行文件或者pip更新检测
 	time.Sleep(time.Second * time.Duration(2))
 	logger.Info("依赖更新成功")
 }
 
-//syncCookieCloud 同步cookie
+// healthCheck 健康检查
+func healthCheck(c *config.Config) {
+	//todo 核心服务(cookiecloud,webdav,ai)健康检查
+	utils.NetworkHealth("健康检查成功")
+	return
+}
+
+// syncCookieCloud 同步cookie
 func syncCookieCloud(c *config.Config) {
 	//todo 定期从cookiecloud获取cookie数据并解密为cookie文件
 	//   *  更新:  cookiecloud->处理成各个音乐平台的cookie数据->储存到本地(覆盖),以平台名称命名
