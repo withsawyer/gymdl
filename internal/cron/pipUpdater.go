e94f7a12413be32aff6517822b1590f64aabe9ea
nichuanfang <f18326186224@gmail.com>
2025-10-24 10:19:16 +0800

    feat: 通过pip安装依赖

--- internal/cron/pipUpdater.go
+++ internal/cron/pipUpdater.go
@@ -1,12 +1,20 @@
 package cron
 
-import "os/exec"
+import (
+    "os/exec"
+)
 
 // installPipDependency 安装pip依赖(执行一次)
 func installPipDependency() {
 	// 通过pip安装/更新
-	exec.Command("pip", "install", "-U", "-r", "requirements.txt")
-	logger.Info("【Pip Dependency】 has Installed.")
+	command := exec.Command("pip", "install", "-U", "-r", "requirements.txt")
+	if output,err := command.CombinedOutput(); err != nil {
+        logger.Info("\n"+string(output))
+		logger.Error("【Pip Dependency】Installed failed")
+	} else {
+        logger.Info("\n"+string(output))
+		logger.Info("【Pip Dependency】has Installed.")
+	}
 }
 
 // updateByRequirements 检查pip依赖更新(定时执行)
