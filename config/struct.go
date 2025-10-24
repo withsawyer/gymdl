d6c1c110060ac0e5823940d46014e2e9a2fd1d4c
nichuanfang <f18326186224@gmail.com>
2025-10-24 08:46:10 +0800

    1

--- config/struct.go
+++ config/struct.go
@@ -1,65 +1,65 @@
 package config
 
 type Config struct {
-    WebConfig   *WebConfig         `json:"web_config"`   // web配置
-    CookieCloud *CookieCloudConfig `json:"cookie_cloud"` // cookiecloud配置
-    MusicTidy   *MusicTidyConfig   `json:"music_tidy"`   // 音乐整理配置
-    WebDAV      *WebDAVConfig      `json:"webdav"`       // webdav配置
-    Ffmpeg      *FfmpegConfig      `json:"ffmpeg"`       // ffmpeg配置，转码使用
-    Log         *LogConfig         `json:"log"`          // 日志配置
-    Telegram    TelegramConfig     `json:"telegram"`     // telegram配置
-    AI          *AIConfig          `json:"ai"`           // AI配置
+	WebConfig   *WebConfig         `json:"web_config"`   // web配置
+	CookieCloud *CookieCloudConfig `json:"cookie_cloud"` // cookiecloud配置
+	MusicTidy   *MusicTidyConfig   `json:"music_tidy"`   // 音乐整理配置
+	WebDAV      *WebDAVConfig      `json:"webdav"`       // webdav配置
+	Ffmpeg      *FfmpegConfig      `json:"ffmpeg"`       // ffmpeg配置，转码使用
+	Log         *LogConfig         `json:"log"`          // 日志配置
+	Telegram    TelegramConfig     `json:"telegram"`     // telegram配置
+	AI          *AIConfig          `json:"ai"`           // AI配置
 }
 
 type WebConfig struct {
-    AppDomain string `json:"app_domain"` // web服务domain
-    Https     bool   `json:"https"`      // 是否开启了 https
-    AppPort   int    `json:"app_port"`   // web服务监听端口
-    GinMode   string `json:"gin_mode"`   // Gin的运行模式: 可选项[debug release test]
+	AppDomain string `json:"app_domain"` // web服务domain
+	Https     bool   `json:"https"`      // 是否开启了 https
+	AppPort   int    `json:"app_port"`   // web服务监听端口
+	GinMode   string `json:"gin_mode"`   // Gin的运行模式: 可选项[debug release test]
 }
 
 type CookieCloudConfig struct {
-    CookieCloudUrl  string `json:"cookiecloud_url"`  // cookiecloud 地址
-    CookieCloudUUID string `json:"cookiecloud_uuid"` // cookiecloud uuid
-    CookieCloudKEY  string `json:"cookiecloud_key"`  // cookiecloud key
-    CookieFile      string `json:"cookie_file"`      // cookie文件名
-    CookieFilePath  string `json:"cookie_file_path"` // cookie存储目录
-    ExpireTime      int    `json:"expire_time"`      // cookie文件过期时间(分钟)
+	CookieCloudUrl  string `json:"cookiecloud_url"`  // cookiecloud 地址
+	CookieCloudUUID string `json:"cookiecloud_uuid"` // cookiecloud uuid
+	CookieCloudKEY  string `json:"cookiecloud_key"`  // cookiecloud key
+	CookieFile      string `json:"cookie_file"`      // cookie文件名
+	CookieFilePath  string `json:"cookie_file_path"` // cookie存储目录
+	ExpireTime      int    `json:"expire_time"`      // cookie文件过期时间(分钟)
 }
 
 type MusicTidyConfig struct {
-    Mode    int    `json:"mode"`     // 音乐整理模式: 1整理到DistDir, 2整理到webdav目录WebDAVDir
-    DistDir string `json:"dist_dir"` // 整理到的路径,仅在Mode为1时使用该目录
+	Mode    int    `json:"mode"`     // 音乐整理模式: 1整理到DistDir, 2整理到webdav目录WebDAVDir
+	DistDir string `json:"dist_dir"` // 整理到的路径,仅在Mode为1时使用该目录
 }
 
 type WebDAVConfig struct {
-    WebDAVUrl  string `json:"webdav_url"`  // webdav地址
-    WebDAVUser string `json:"webdav_user"` // webdav用户名
-    WebDAVPass string `json:"webdav_pass"` // webdav密码
-    WebDAVDir  string `json:"webdav_dir"`  // wevdav路径
+	WebDAVUrl  string `json:"webdav_url"`  // webdav地址
+	WebDAVUser string `json:"webdav_user"` // webdav用户名
+	WebDAVPass string `json:"webdav_pass"` // webdav密码
+	WebDAVDir  string `json:"webdav_dir"`  // wevdav路径
 }
 
 type FfmpegConfig struct {
-    MaxWorker   int    `json:"max_worker"`   // 最大进程数：建议为逻辑CPU个数
-    FfmpegPath  string `json:"ffmpeg_path"`  // ffmpeg 可执行文件路径
-    FfprobePath string `json:"ffprobe_path"` // ffprobe 可执行文件路径
+	MaxWorker   int    `json:"max_worker"`   // 最大进程数：建议为逻辑CPU个数
+	FfmpegPath  string `json:"ffmpeg_path"`  // ffmpeg 可执行文件路径
+	FfprobePath string `json:"ffprobe_path"` // ffprobe 可执行文件路径
 }
 
 type LogConfig struct {
-    Mode  int    `json:"mode"`  // 日志模式：1标准输出，2日志文件，3标准输出和日志文件
-    Level int    `json:"level"` // 日志等级，1=debug 2=info 3=warn 4=error 5=fatal
-    File  string `json:"file"`  // 日志文件路径
+	Mode  int    `json:"mode"`  // 日志模式：1标准输出，2日志文件，3标准输出和日志文件
+	Level int    `json:"level"` // 日志等级，1=debug 2=info 3=warn 4=error 5=fatal
+	File  string `json:"file"`  // 日志文件路径
 }
 
 type TelegramConfig struct {
-    Mode     int    `json:"mode"`      // 运行模式: 1长轮询, 2Webhook   开发环境推荐1,生产环境推荐2
-    ChatID   string `json:"chat_id"`   // chat_id
-    BotToken string `json:"bot_token"` // telegram机器人token
+	Mode     int    `json:"mode"`      // 运行模式: 1长轮询, 2Webhook   开发环境推荐1,生产环境推荐2
+	ChatID   string `json:"chat_id"`   // chat_id
+	BotToken string `json:"bot_token"` // telegram机器人token
 }
 
 type AIConfig struct {
-    BaseUrl      string `json:"base_url"`      // baseurl
-    Model        string `json:"response"`      // 使用的模型
-    ApiKey       string `json:"api_key"`       // apiKey
-    SystemPrompt string `json:"system_prompt"` // 默认系统提示词
+	BaseUrl      string `json:"base_url"`      // baseurl
+	Model        string `json:"response"`      // 使用的模型
+	ApiKey       string `json:"api_key"`       // apiKey
+	SystemPrompt string `json:"system_prompt"` // 默认系统提示词
 }
--- main.go
+++ main.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "context"
-    "errors"
-    "flag"
-    "fmt"
-    "net/http"
-    "os"
-    "os/signal"
-    "runtime"
-    "sync"
-    "syscall"
-    "time"
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"sync"
+	"syscall"
+	"time"
 
-    "github.com/gin-gonic/gin"
-    "github.com/nichuanfang/gymdl/config"
-    "github.com/nichuanfang/gymdl/internal/cron"
-    "github.com/nichuanfang/gymdl/internal/gin/router"
-    "github.com/nichuanfang/gymdl/utils"
-    "go.uber.org/zap"
+	"github.com/gin-gonic/gin"
+	"github.com/nichuanfang/gymdl/config"
+	"github.com/nichuanfang/gymdl/internal/cron"
+	"github.com/nichuanfang/gymdl/internal/gin/router"
+	"github.com/nichuanfang/gymdl/utils"
+	"go.uber.org/zap"
 )
 
 var (
-    configFile   string
-    version      bool
-    buildVersion = "dev-main"
+	configFile   string
+	version      bool
+	buildVersion = "dev-main"
 )
 
 func init() {
-    flag.StringVar(&configFile, "c", "./config.json", "config file")
-    flag.BoolVar(&version, "v", false, "display version")
-    flag.Parse()
+	flag.StringVar(&configFile, "c", "./config.json", "config file")
+	flag.BoolVar(&version, "v", false, "display version")
+	flag.Parse()
 }
 
 // =================================基础服务================================================
 
 // printBanner 打印启动横幅
 func printBanner() {
-    green := "\033[32m"
-    reset := "\033[0m"
+	green := "\033[32m"
+	reset := "\033[0m"
 
-    banner := `
+	banner := `
    ________   _ _       ____   _    
   /  __/\  \/// \__/|  /  _ \ / \   
   | |  _ \  / | |\/||  | | \|| |   
@@ -50,131 +50,131 @@ func printBanner() {
           🚀 Service Starting...
 ==========================================
 `
-    fmt.Println(green + banner + reset)
+	fmt.Println(green + banner + reset)
 }
 
 // initWebDAV 初始化webdav服务
 func initWebDAV(c *config.WebDAVConfig) {
-    utils.ServiceIsOnf("已加载webdav服务")
-    return
+	utils.ServiceIsOnf("已加载webdav服务")
+	return
 }
 
 // initCookieCloud 初始化cookiecloud
 func initCookieCloud(cookieCloudConfig *config.CookieCloudConfig) {
-    utils.ServiceIsOnf("已加载cookiecloud服务")
+	utils.ServiceIsOnf("已加载cookiecloud服务")
 }
 
 // initAI 初始化AI服务
 func initAI(c *config.AIConfig) {
-    utils.ServiceIsOnf("已加载AI服务")
+	utils.ServiceIsOnf("已加载AI服务")
 }
 
 // =================================后台服务================================================
 
 // initCron 启动定时任务
 func initCron(ctx context.Context, wg *sync.WaitGroup, c *config.Config) {
-    defer wg.Done()
-    s := cron.InitScheduler(c)
-    s.Start()
-    utils.Success("Scheduler is started")
-    <-ctx.Done()
-    utils.Stop("定时任务已关闭")
+	defer wg.Done()
+	s := cron.InitScheduler(c)
+	s.Start()
+	utils.Success("Scheduler is started")
+	<-ctx.Done()
+	utils.Stop("定时任务已关闭")
 }
 
 // initGin 启动Web服务
 func initGin(ctx context.Context, wg *sync.WaitGroup, c *config.Config) {
-    defer wg.Done()
-    // 设置运行模式 debug/release/test
-    gin.SetMode(c.WebConfig.GinMode)
-    r := router.SetupRouter(c)
+	defer wg.Done()
+	// 设置运行模式 debug/release/test
+	gin.SetMode(c.WebConfig.GinMode)
+	r := router.SetupRouter(c)
 
-    srv := &http.Server{
-        Addr:    fmt.Sprintf(":%d", c.WebConfig.AppPort),
-        Handler: r,
-    }
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", c.WebConfig.AppPort),
+		Handler: r,
+	}
 
-    go func() {
-        var httpFlag string
-        if c.WebConfig.Https {
-            httpFlag = "https"
-        } else {
-            httpFlag = "http"
-        }
-        utils.Successf(fmt.Sprintf("Gin server is starting on %s://%s:%d", httpFlag, c.WebConfig.AppDomain, c.WebConfig.AppPort))
-        if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-            utils.Logger().Error("Gin server error", zap.Any("error", err))
-        }
-    }()
-    <-ctx.Done()
-    utils.Stop("Gin服务已关闭")
-    shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
-    if err := srv.Shutdown(shutdownCtx); err != nil {
-        utils.Logger().Error("Gin服务关闭错误", zap.Any("error", err))
-    }
+	go func() {
+		var httpFlag string
+		if c.WebConfig.Https {
+			httpFlag = "https"
+		} else {
+			httpFlag = "http"
+		}
+		utils.Successf(fmt.Sprintf("Gin server is starting on %s://%s:%d", httpFlag, c.WebConfig.AppDomain, c.WebConfig.AppPort))
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			utils.Logger().Error("Gin server error", zap.Any("error", err))
+		}
+	}()
+	<-ctx.Done()
+	utils.Stop("Gin服务已关闭")
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		utils.Logger().Error("Gin服务关闭错误", zap.Any("error", err))
+	}
 }
 
 // initGin 启动tg机器人
 func initBot(ctx context.Context, wg *sync.WaitGroup, c *config.Config) {
-    defer wg.Done()
-    // todo 执行启动操作
-    utils.Success("Telegram Bot is started")
-    <-ctx.Done()
-    utils.Stop("Telegram Bot 退出")
+	defer wg.Done()
+	// todo 执行启动操作
+	utils.Success("Telegram Bot is started")
+	<-ctx.Done()
+	utils.Stop("Telegram Bot 退出")
 }
 
 // =====================================程序入口================================================
 
 func main() {
-    if version {
-        fmt.Printf("version: %s, build with: %s\n", buildVersion, runtime.Version())
-        return
-    }
-    // banner
-    printBanner()
+	if version {
+		fmt.Printf("version: %s, build with: %s\n", buildVersion, runtime.Version())
+		return
+	}
+	// banner
+	printBanner()
 
-    // 加载配置
-    c := config.LoadConfig(configFile)
+	// 加载配置
+	c := config.LoadConfig(configFile)
 
-    // 初始化日志模块
-    err := utils.InitLogger(c.Log)
-    if err != nil {
-        return
-    }
-    defer utils.Sync()
+	// 初始化日志模块
+	err := utils.InitLogger(c.Log)
+	if err != nil {
+		return
+	}
+	defer utils.Sync()
 
-    // 初始化webdav+连通性检测
-    if c.MusicTidy.Mode == 2 {
-        initWebDAV(c.WebDAV)
-    }
-    // 初始化cookiecloud+连通性检测
-    initCookieCloud(c.CookieCloud)
+	// 初始化webdav+连通性检测
+	if c.MusicTidy.Mode == 2 {
+		initWebDAV(c.WebDAV)
+	}
+	// 初始化cookiecloud+连通性检测
+	initCookieCloud(c.CookieCloud)
 
-    // 初始化AI服务+连通性检测
-    initAI(c.AI)
+	// 初始化AI服务+连通性检测
+	initAI(c.AI)
 
-    // 创建可取消上下文
-    ctx, cancel := context.WithCancel(context.Background())
+	// 创建可取消上下文
+	ctx, cancel := context.WithCancel(context.Background())
 
-    wg := &sync.WaitGroup{}
-    wg.Add(3)
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
 
-    // 【协程1】 启动定时任务
-    go initCron(ctx, wg, c)
-    // 【协程2】 启动web服务Gin
-    go initGin(ctx, wg, c)
-    // 【协程3】 启动telegram机器人
-    go initBot(ctx, wg, c)
+	// 【协程1】 启动定时任务
+	go initCron(ctx, wg, c)
+	// 【协程2】 启动web服务Gin
+	go initGin(ctx, wg, c)
+	// 【协程3】 启动telegram机器人
+	go initBot(ctx, wg, c)
 
-    // 捕捉系统信号，优雅退出
-    sig := make(chan os.Signal, 1)
-    signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-    // 文丑丑
-    <-sig
-    utils.Logger().Info("收到退出信号，开始关闭服务...")
-    cancel() // 通知所有协程退出
+	// 捕捉系统信号，优雅退出
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	// 文丑丑
+	<-sig
+	utils.Logger().Info("收到退出信号，开始关闭服务...")
+	cancel() // 通知所有协程退出
 
-    // 阻塞主协程
-    wg.Wait()
-    utils.Logger().Info("所有服务已退出，程序结束")
+	// 阻塞主协程
+	wg.Wait()
+	utils.Logger().Info("所有服务已退出，程序结束")
 }
