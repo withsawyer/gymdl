8cce080021ac63d0149d30b7725f06136e812ae7
nichuanfang <f18326186224@gmail.com>
2025-10-24 08:49:05 +0800

    refactor: format code

--- internal/cron/pipUpdater.go
+++ internal/cron/pipUpdater.go
@@ -4,12 +4,12 @@ import "os/exec"
 
 // installPipDependency 安装pip依赖(执行一次)
 func installPipDependency() {
-	//通过pip安装/更新
-	exec.Command("pip", "install", "-U", "-r", "requirements.txt")
-	logger.Info("【Pip Dependency】 has Installed.")
+    // 通过pip安装/更新
+    exec.Command("pip", "install", "-U", "-r", "requirements.txt")
+    logger.Info("【Pip Dependency】 has Installed.")
 }
 
 // updateByRequirements 检查pip依赖更新(定时执行)
 func updatePipDependency() {
-	//通过pip更新
+    // 通过pip更新
 }
