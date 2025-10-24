fe4a4913e5b4b1d76113ce507d5f5801a27297b1
nichuanfang <f18326186224@gmail.com>
2025-10-24 08:51:39 +0800

    refactor: format code

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -1,11 +1,11 @@
 package bot
 
 import (
-	"time"
-	
-	"github.com/nichuanfang/gymdl/config"
-	"github.com/nichuanfang/gymdl/utils"
-	"go.uber.org/zap"
+    "time"
+
+    "github.com/nichuanfang/gymdl/config"
+    "github.com/nichuanfang/gymdl/utils"
+    "go.uber.org/zap"
 )
 
 // tg机器人入口
@@ -13,8 +13,8 @@ import (
 var logger *zap.Logger
 
 func InitBot(c *config.Config) {
-	logger = utils.Logger()
-	utils.Success("TelegramBot已成功启动")
-	// 模拟阻塞
-	time.Sleep(time.Hour)
+    logger = utils.Logger()
+    utils.Success("TelegramBot已成功启动")
+    // 模拟阻塞
+    time.Sleep(time.Hour)
 }
