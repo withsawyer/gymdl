2f533fdb25eff528e3e3efa0a881504342757c72
nichuanfang <f18326186224@gmail.com>
2025-10-30 21:29:27 +0800

    fix: 修复项目启动报错

--- internal/bot/dispatch/constant.go
+++ internal/bot/dispatch/constant.go
@@ -4,18 +4,16 @@ import (
 	"time"
 
 	"github.com/nichuanfang/gymdl/config"
-	"github.com/nichuanfang/gymdl/core/domain"
 	tb "gopkg.in/telebot.v4"
 )
 
 type Session struct {
-	Text     string          // 用户发送的消息
-	Context  tb.Context      // tg上下文
-	User     *tb.User        // 用户
-	Bot      tb.API          // 机器人
-	Msg      *tb.Message     // 初始化消息对象
-	Link     string          // 有效链接
-	LinkType domain.LinkType // 链接类型
-	Start    time.Time       // 开始处理时间
-	Cfg      *config.Config  // 配置文件
+	Text    string         // 用户发送的消息
+	Context tb.Context     // tg上下文
+	User    *tb.User       // 用户
+	Bot     tb.API         // 机器人
+	Msg     *tb.Message    // 初始化消息对象
+	Link    string         // 有效链接
+	Start   time.Time      // 开始处理时间
+	Cfg     *config.Config // 配置文件
 }
