367217cbdd0c58a9c5a8d3bd43e492fd4d2a2b05
nichuanfang <f18326186224@gmail.com>
2025-11-02 15:27:11 +0800

    perf: 嵌入的封面图缩放图片到 320x320px

--- go.mod
+++ go.mod
@@ -30,6 +30,7 @@ require (
 	github.com/bytedance/sonic v1.14.1 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -55,6 +56,7 @@ require (
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.22.0 // indirect
 	golang.org/x/crypto v0.43.0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/mod v0.29.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
--- utils/pic.go
+++ utils/pic.go
@@ -1,13 +1,20 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
-	"io"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"image/png"
 	"net/http"
+	"strings"
+
+	"github.com/disintegration/imaging"
 )
 
 // FetchImage 下载图片并返回字节数组
-func FetchImage(url string) ([]byte, error) {
+func FetchAndResizeImage(url string) ([]byte, error) {
 	if url == "" {
 		return nil, fmt.Errorf("empty URL")
 	}
@@ -22,10 +29,31 @@ func FetchImage(url string) ([]byte, error) {
 		return nil, fmt.Errorf("failed to fetch image: status code %d", resp.StatusCode)
 	}
 
-	imageData, err := io.ReadAll(resp.Body)
+	img, format, err := image.Decode(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read image body: %w", err)
+		return nil, fmt.Errorf("failed to decode image: %w", err)
 	}
 
-	return imageData, nil
+	// 使用高性能的缩放算法
+	resized := imaging.Fit(img, 320, 320, imaging.Lanczos)
+
+	// 创建黑底 320x320
+	dst := imaging.New(320, 320, color.NRGBA{0, 0, 0, 255})
+
+	// 居中绘制缩放后的图片
+	dst = imaging.PasteCenter(dst, resized)
+
+	// 编码输出为 JPEG
+	var buf bytes.Buffer
+	switch strings.ToLower(format) {
+	case "png":
+		err = png.Encode(&buf, dst)
+	default:
+		err = jpeg.Encode(&buf, dst, &jpeg.Options{Quality: 90})
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode resized image: %w", err)
+	}
+
+	return buf.Bytes(), nil
 }
